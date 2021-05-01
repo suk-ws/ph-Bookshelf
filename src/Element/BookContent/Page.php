55610855f446ea0ee96dc18c2fdf999409c41ae3
Eyre_S <Sukazyo@outlook.com>
2021-05-01 15:48:12 +0800

    修复 page 的指向链接中没有添加 bookId 的问题

--- src/Element/BookContent/Page.php
+++ src/Element/BookContent/Page.php
@@ -72,7 +72,16 @@ class Page {
 	}
 	
 	public function getSummaryHtml (): string {
-		$str = "<li class='page-contented chapter" . (PageMeta::$page->getId()==$this->id?" active":"") . "'><a class='page-contented' " . (PageMeta::$page->getId()==$this->id?"":(PageMeta::$book->getId()." href='/".$this->id."'")) . ">$this->name</a>";
+		$str =
+			"<li class='page-contented chapter" .
+			(PageMeta::$page->getId()==$this->id?" active":"") .
+			"'><a class='page-contented' " .
+			(
+				PageMeta::$page->getId()==$this->id ?
+				"" :
+				"href='/".PageMeta::$book->getId()."/".$this->id."' "
+			) .
+			">$this->name</a>";
 		if (sizeof($this->segues) > 0) {
 			$str .= "<ul class='page-contented summary'>";
 			foreach ($this->segues as $node) {
