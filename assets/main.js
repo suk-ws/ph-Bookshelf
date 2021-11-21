0024f32fb97107b419cc2a286885d8a1d580cedf
Eyre_S <sukazyo@outlook.com>
2021-11-21 18:41:24 +0800

    侧边栏和基础元素动画补充
    - 在主要区块左上角添加一个侧边栏开关按钮
    - 侧边栏动画实现
      - 实现了侧边栏开关和侧边栏滑入滑出动画
      - 菜单物体悬浮颜色动画
      - 菜单物体的已选中区块指示器实现
      - 子菜单的开关动画
    - 补全 a 标签的样式和动画
    - 重置了默认 button 样式

--- assets/main.js
+++ assets/main.js
@@ -1,10 +1,45 @@
 
+const cssVar = getComputedStyle(document.querySelector(":root"));
+
+const animationSpeedMultiplier = parseFloat(cssVar.getPropertyValue("--animation-speed-multiplier"));
+const menuItemChildrenAnimaSpeed = parseFloat(cssVar.getPropertyValue("--sidebar-menu-item-anima-speed")) * 1000 * animationSpeedMultiplier;
+
+const itemSidebar = document.getElementById("sidebar");
+
 for (const node of document.getElementsByTagName("noscript")) {
 	node.parentNode.removeChild(node);
 }
 
 for (const node of document.getElementsByClassName("menu-item-parent")) {
-	node.firstElementChild.onclick = function () {
+	const children = node.getElementsByClassName("children")[0];
+	node.firstElementChild.onclick = async function () {
 		node.classList.toggle("active");
+		if (node.classList.contains("active")) {
+			const height = children.clientHeight;
+			children.style.height = "0px";
+			await sleep(1);
+			children.style.height = height + "px";
+			await sleep(menuItemChildrenAnimaSpeed);
+			children.style.height = "";
+		} else {
+			children.style.display = "block";
+			const height = children.clientHeight;
+			children.style.height = height + "px";
+			await sleep(1);
+			children.style.height = "0px";
+			await sleep(menuItemChildrenAnimaSpeed);
+			children.style.height = "";
+			children.style.display = "";
+		}
 	}
 }
+
+function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+document.getElementById("sidebar-show").onclick = sidebarToggle;
+function sidebarToggle() {
+	itemSidebar.parentElement.classList.toggle("show-sidebar");
+}
+sidebarToggle();
